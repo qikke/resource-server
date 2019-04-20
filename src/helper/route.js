d3222a887212126f29a6058c758b072ac58d5bd9
qikai <qk098@qimingpian.com>
2019-04-21 01:55:23 +0800

    add mime

--- src/helper/route.js
+++ src/helper/route.js
@@ -5,6 +5,7 @@ const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 const config = require('../config/defaultConfig');
+const mime = require('./mime');
 
 const tplPath = path.join(__dirname, '../template/dir.html');
 const source = fs.readFileSync(tplPath);
@@ -14,13 +15,15 @@ module.exports = async function (req, res, filePath) {
   try {
     const stats = await stat(filePath);
     if (stats.isFile()) {
+      const contentType = mime(filePath);
       res.statusCode = 200;
-      res.setHeader('Content-type', 'text/plain');
+      res.setHeader('Content-type', contentType);
       fs.createReadStream(filePath).pipe(res);
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath);
       res.statusCode = 200;
       res.setHeader('Content-type', 'text/html');
+      // find relative path
       const dir = path.relative(config.root, filePath)
       const data = {
         title: path.basename(filePath),
