b7cb48c104fe3aae9f30f3ae77a02074154d8733
qikai <qk098@qimingpian.com>
2019-04-21 01:32:56 +0800

    add route

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 const http = require('http');
 const conf = require('./config/defaultConfig');
-const chalk = require('chalk');
+const path = require('path');
+const route = require('./helper/route');
 
-const server = http.createServer((req, res)=>{
-  res.statusCode = 200;
-  res.setHeader('Content-type', 'text/html');
-  res.write('<html><body><div style="color: red;">HELLO,</div></body></html>');
-  res.end();
+const server = http.createServer((req, res) => {
+  const filePath = path.join(conf.root, req.url);
+  route(req, res, filePath);
 });
 
-server.listen(conf.port, conf.hostname, ()=>{
+server.listen(conf.port, conf.hostname, () => {
   const addr = `http://${conf.hostname}:${conf.port}`;
-  console.log(`Server started at ${chalk.green(addr)}`);
+  console.log(`Server started at ${addr}`);
 });
-
--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 module.exports = {
   hostname: '127.0.0.1',
-  port: 9527
+  port: 9527,
+  root: path.resolve('./')
 };
