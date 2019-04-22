1ecae2c0c862f0bf6487053736fa575569790345
qikai <qk098@qimingpian.com>
2019-04-22 08:41:41 +0800

    add compress

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -3,5 +3,7 @@ const path = require('path');
 module.exports = {
   hostname: '127.0.0.1',
   port: 9527,
-  root: path.resolve('./')
+  root: path.resolve('./'),
+  // 需要压缩的文件格式
+  compress: /\.(html|js|css|md)/
 };
